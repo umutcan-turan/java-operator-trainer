9ef75be0f8f9dfb64c7944ed768060f9aa306f5f
Umutcan Turan <mail@umutcan.org>
2024-04-14 19:55:23 +0300

    Use ArrayList instead of array

--- src/java_operator_trainer/JavaOperatorTrainerApp.java
+++ src/java_operator_trainer/JavaOperatorTrainerApp.java
@@ -1,6 +1,8 @@
 package java_operator_trainer;
 
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 class JavaOperatorTrainerApp {
 	public static void main(String [] args)
@@ -12,7 +14,7 @@ class JavaOperatorTrainerApp {
 	{
 		Random rand = new Random();
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		Operator [] operators = initializeOperatorsArray();
+		List<Operator> operators = initializeOperatorsArray();
 		
 		
 		
@@ -22,82 +24,82 @@ class JavaOperatorTrainerApp {
 		kb.close();
 	}
 
-	public static Operator [] initializeOperatorsArray()
+	public static List<Operator> initializeOperatorsArray()
 	{
-		Operator [] arr = new Operator[25];
+		List<Operator> arr = new ArrayList<Operator>();
 		
 		// Based on 006-Temel-Operatorler-V2.0.pdf from csystem.org
 		
 		// TODO: Fix and uncomment the operators as they are covered in the course.
 		
 		//                       Name               Atom    Lvl Op  Prefix  Infix  Postfix LeftToRight	
-		//arr[xx] = new Operator("",                "()",   1,  x,  x,      x,      x,      x);
-		//arr[xx] = new Operator("",                ".",    1,  x,  x,      x,      x,      x);
-		//arr[xx] = new Operator("",                "[]",   1,  x,  x,      x,      x,      x);
-		//arr[xx] = new Operator("",               "new",   1,  x,  x,      x,      x,      x);
+		//arr.add(new Operator("",                "()",   1,  x,  x,      x,      x,      x));
+		//arr.add(new Operator("",                ".",    1,  x,  x,      x,      x,      x));
+		//arr.add(new Operator("",                "[]",   1,  x,  x,      x,      x,      x));
+		//arr.add(new Operator("",               "new",   1,  x,  x,      x,      x,      x));
 		
-		arr[0] = new Operator("İşaret artı",        "+",    2,  1,  true,   false,  false,  false);
-		arr[1] = new Operator("İşaret eksi",        "-",    2,  1,  true,   false,  false,  false);
-		arr[2] = new Operator("Artırma",            "++",   2,  1,  true,   false,  true,   false);
-		arr[3] = new Operator("Azaltma",            "--",   2,  1,  true,   false,  true,   false);
-		arr[4] = new Operator("Mantıksal değil",    "!",    2,  1,  true,   false,  false,  false);
-		//arr[xx] = new Operator("Bitsel değil",    "~",    2,  x,  x,      x,      x,      x);
-		//arr[xx] = new Operator("",                "()",   2,  x,  x,      x,      x,      x);
+		arr.add(new Operator("İşaret artı",        "+",    2,  1,  true,   false,  false,  false));
+		arr.add(new Operator("İşaret eksi",        "-",    2,  1,  true,   false,  false,  false));
+		arr.add(new Operator("Artırma",            "++",   2,  1,  true,   false,  true,   false));
+		arr.add(new Operator("Azaltma",            "--",   2,  1,  true,   false,  true,   false));
+		arr.add(new Operator("Mantıksal değil",    "!",    2,  1,  true,   false,  false,  false));
+		//arr.add(new Operator("",                 "~",    2,  x,  x,      x,      x,      x));
+		//arr.add(new Operator("",                "()",   2,  x,  x,      x,      x,      x));
 		
-		arr[5] = new Operator("Çarpma",             "*",    3,  2,  false,  true,   false,  true);
-		arr[6] = new Operator("Bölme",              "/",    3,  2,  false,  true,   false,  true);
-		arr[7] = new Operator("Mod alma",           "%",    3,  2,  false,  true,   false,  true);
+		arr.add(new Operator("Çarpma",             "*",    3,  2,  false,  true,   false,  true));
+		arr.add(new Operator("Bölme",              "/",    3,  2,  false,  true,   false,  true));
+		arr.add(new Operator("Mod alma",           "%",    3,  2,  false,  true,   false,  true));
 		
 		
-		arr[8] = new Operator("Toplama",            "+",    4,  2,  false,  true,   false,  true);
-		arr[9] = new Operator("Çıkarma",            "-",    4,  2,  false,  true,   false,  true);
+		arr.add(new Operator("Toplama",            "+",    4,  2,  false,  true,   false,  true));
+		arr.add(new Operator("Çıkarma",            "-",    4,  2,  false,  true,   false,  true));
 		
-		//arr[xx] = new Operator("",                "<<",   5,  x,  x,      x,      x,      x);
-		//arr[xx] = new Operator("",                ">>",   5,  x,  x,      x,      x,      x);
-		//arr[xx] = new Operator("",               ">>>",   5,  x,  x,      x,      x,      x);
+		//arr.add(new Operator("",                "<<",   5,  x,  x,      x,      x,      x));
+		//arr.add(new Operator("",                ">>",   5,  x,  x,      x,      x,      x));
+		//arr.add(new Operator("",               ">>>",   5,  x,  x,      x,      x,      x));
 		
-		arr[10] = new Operator("Büyüktür",          ">",    6,  2,  false,  true,   false,  true);
-		arr[11] = new Operator("Küçüktür",          "<",    6,  2,  false,  true,   false,  true);
-		arr[12] = new Operator("Büyük eşittir",    ">=",    6,  2,  false,  true,   false,  true);
-		arr[13] = new Operator("Küçük eşittir",    "<=",    6,  2,  false,  true,   false,  true);
-		//arr[xx] = new Operator("",       "instanceof",    6,  x,  x,      x,      x,      x);
+		arr.add(new Operator("Büyüktür",          ">",    6,  2,  false,  true,   false,  true));
+		arr.add(new Operator("Küçüktür",          "<",    6,  2,  false,  true,   false,  true));
+		arr.add(new Operator("Büyük eşittir",    ">=",    6,  2,  false,  true,   false,  true));
+		arr.add(new Operator("Küçük eşittir",    "<=",    6,  2,  false,  true,   false,  true));
+		//arr.add(new Operator("",       "instanceof",    6,  x,  x,      x,      x,      x));
 		
-		arr[14] = new Operator("Eşittir",          "==",    7,  2,  false,  true,   false,  true);
-		arr[15] = new Operator("Eşit değildir",    "!=",    7,  2,  false,  true,   false,  true);
+		arr.add(new Operator("Eşittir",          "==",    7,  2,  false,  true,   false,  true));
+		arr.add(new Operator("Eşit değildir",    "!=",    7,  2,  false,  true,   false,  true));
 		
-		arr[16] = new Operator("Bitsel ve",         "&",    8,  1,  false,  true,   false,  true);
+		arr.add(new Operator("Bitsel ve",         "&",    8,  1,  false,  true,   false,  true));
 		
-		//arr[xx] = new Operator("",                "^",    9,  x,  x,      x,      x,      x);
+		//arr.add(new Operator("",                "^",    9,  x,  x,      x,      x,      x));
 		
-		arr[17] = new Operator("Bitsel veya",       "|",   10,  1,  false,  true,   false,  true);
+		arr.add(new Operator("Bitsel veya",       "|",   10,  1,  false,  true,   false,  true));
 		
-		arr[18] = new Operator("Mantıksal ve",     "&&",   11,  2,  false,  true,   false,  true);
+		arr.add(new Operator("Mantıksal ve",     "&&",   11,  2,  false,  true,   false,  true));
 		
-		arr[19] = new Operator("Mantıksal veya",   "||",   12,  2,  false,  true,   false,  true);
+		arr.add(new Operator("Mantıksal veya",   "||",   12,  2,  false,  true,   false,  true));
 
-		//arr[xx] = new Operator("",               ":?",   13,  x,  x,      x,      x,      x);
+		//arr.add(new Operator("",               ":?",   13,  x,  x,      x,      x,      x));
 		
-		arr[20] = new Operator("Atama",             "=",   14,  1,  false,  true,   false,  false);
-		arr[21] = new Operator("Çarparak atama",   "*=",   14,  1,  false,  true,   false,  false);
-		arr[22] = new Operator("Bölerek atama",    "/=",   14,  1,  false,  true,   false,  false);
+		arr.add(new Operator("Atama",             "=",   14,  1,  false,  true,   false,  false));
+		arr.add(new Operator("Çarparak atama",   "*=",   14,  1,  false,  true,   false,  false));
+		arr.add(new Operator("Bölerek atama",    "/=",   14,  1,  false,  true,   false,  false));
 
-		arr[23] = new Operator("Toplayarak atama", "+=",   14,  1,  false,  true,   false,  false);
-		arr[24] = new Operator("Çıkararak atama",  "-=",   14,  1,  false,  true,   false,  false);
+		arr.add(new Operator("Toplayarak atama", "+=",   14,  1,  false,  true,   false,  false));
+		arr.add(new Operator("Çıkararak atama",  "-=",   14,  1,  false,  true,   false,  false));
 		
 		return arr;
 	}
 }
 
 class PrecedenceChecker {
-	public static void loop(Operator [] operators, Random rand, java.util.Scanner kb)
+	public static void loop(List<Operator> operators, Random rand, java.util.Scanner kb)
 	{
 		System.out.println("Çıkış için 0 giriniz");
 		
 		while (true) {
-			int idx1 = rand.nextInt(operators.length);
-			int idx2 = generateIdx2(idx1, operators.length, rand);
-			Operator op1 = operators[idx1];
-			Operator op2 = operators[idx2];
+			int idx1 = rand.nextInt(operators.size());
+			int idx2 = generateIdx2(idx1, operators.size(), rand);
+			Operator op1 = operators.get(idx1);
+			Operator op2 = operators.get(idx2);
 			int answer;
 			boolean isCorrect;
 			
